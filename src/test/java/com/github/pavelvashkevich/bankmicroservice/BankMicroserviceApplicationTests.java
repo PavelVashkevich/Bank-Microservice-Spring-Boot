02e5f2f61143cf156933a33fcc73ee4c64e78fd9
Pavel Vashkevich <veider300@gmail.com>
2023-03-16 23:25:03 +0100

    Initial commit

---
+++ src/test/java/com/github/pavelvashkevich/bankmicroservice/BankMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.pavelvashkevich.bankmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
